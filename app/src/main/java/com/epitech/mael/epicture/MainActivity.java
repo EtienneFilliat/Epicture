7b2ea0508a0fc7df05ffc29f1c7d2f2c9001e7c9
mael.drapier <mael.drapier@epitech.eu>
2018-11-06 15:04:09 +0100

    #5 - adding correct icons and menu titles

--- app/src/main/java/com/epitech/mael/epicture/MainActivity.java
+++ app/src/main/java/com/epitech/mael/epicture/MainActivity.java
@@ -85,13 +85,13 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.nav_my_pictures) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_search) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_upload) {
+
+        } else if (id == R.id.nav_manage_favorites) {
 
         } else if (id == R.id.nav_logout) {
             CookieManager cookieManager = CookieManager.getInstance();
