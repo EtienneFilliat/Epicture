909f155ee71370c0ac4f78e6ceb2720aec40fbf7
mael.drapier <mael.drapier@epitech.eu>
2018-11-14 12:42:47 +0100

    #26 - graphics improvements

--- app/src/main/java/com/epitech/mael/epicture/Adapters/UserImagesAdaptater.kt
+++ app/src/main/java/com/epitech/mael/epicture/Adapters/UserImagesAdaptater.kt
@@ -12,6 +12,7 @@ import com.epitech.mael.epicture.Imgur.ApiHandler
 import com.epitech.mael.epicture.Imgur.ImageList
 import com.epitech.mael.epicture.Imgur.Response
 import android.support.v7.app.AlertDialog
+import android.widget.Adapter
 import android.widget.Toast
 import kotlinx.android.synthetic.main.user_images_manager.view.*
 import retrofit2.Call
@@ -80,6 +81,7 @@ class UserImagesAdaptater(private val data: List<ImageList.Image>, private val t
                         ApiHandler().getService(token, null).deleteImage(image.id).enqueue(object : retrofit2.Callback<Response> {
                             override fun onResponse(call: Call<Response>, response: retrofit2.Response<Response>) {}
                             override fun onFailure(call: Call<Response>, t: Throwable) { Log.w("DeleteImage", "Failed to delete an image") }
+
                         })
                         Toast.makeText(holder.view.context,  "Image deleted",
                                 Toast.LENGTH_SHORT).show()
